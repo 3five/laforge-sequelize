90c0183e7946c39b775b3fc1c1ef0d83ce350320
Jon Jaques <jon@3five.com>
2016-05-15 16:02:41 -0500

    Add options to findAll and new search endpoint

--- src/collection-router.js
+++ src/collection-router.js
@@ -1,5 +1,6 @@
 import {Router, route} from 'laforge'
-import {merge} from 'lodash'
+import JSON5 from 'json5'
+import {merge, reduce} from 'lodash'
 import {pluralize, capitalize, underscore} from 'inflection'
 
 export default class CollectionRouter extends Router {
@@ -17,25 +18,23 @@ export default class CollectionRouter extends Router {
   
   @route('get', '/')
   getAll(opts, http) {
-    let defaults = {
-      page: 0,
-      limit: 50,
-      include: [],
-      where: {},
-      order: []
+    let query = this.getQueryFromParams(opts)
+    return this.model.findAll(query)
+  }
+
+  @route('get', '/search') 
+  search(opts, http) {
+    let query = this.getQueryFromParams(opts)
+    let fields = opts.query.fields ? opts.query.fields.split(',') : []
+    let queryStr = opts.query.query
+    let where = merge(query.where, { $or: {} })
+    if (queryStr) {
+      query.where = reduce(fields, (m, field)=> {
+        m.$or[field] = { $iLike: `%${queryStr}%` }
+        return m
+      }, where)      
     }
-    let o = merge({}, defaults, opts.query)    
-    let limit = o.limit
-    let offset = (o.page === 0 || o.page === 1) ? 0 : (limit * o.page)
-    let include = this.getAssociations(this.parseInclude(o.include || []))
-    let { where, order } = o
-    return this.model.findAll({
-      limit,
-      offset,
-      include,
-      where,
-      order
-    })
+    return this.model.findAll(query)
   }
 
   @route('post', '/')
@@ -52,7 +51,18 @@ export default class CollectionRouter extends Router {
 
   @route('put', '/:id')
   update(opts, http) {
-    return this.model.updateById(opts.params.id, opts.data)
+    delete opts.data.id
+    return this.model.update(opts.data, {
+      returning: true,
+      where: { id: opts.params.id }
+    }).then(([rowsAffected, data]) => {
+      if (rowsAffected === 1) {
+        return data[0]
+      } else if (rowsAffected > 1) {
+        return data
+      }
+      return null
+    })
   }
 
   @route('delete', '/:id')
@@ -71,6 +81,62 @@ export default class CollectionRouter extends Router {
   getCollectionName() {
     let constructorName = Object.getPrototypeOf(this).constructor.name
     return capitalize(underscore(constructorName).split('_')[0])
+  }
+  getQueryFromParams(opts) {    
+    let defaults = {
+      page: 0,
+      limit: 50,
+      include: [],
+      where: null,
+      order: null
+    }
+    let o = merge({}, defaults, opts.query)    
+    let limit = o.limit
+    let offset = (o.page === 0 || o.page === 1) ? 0 : (limit * o.page)
+    let include = this.getAssociations(this.parseInclude(o.include || []))
+    let where = this.parseWhere(o.where || '')
+    let order = this.parseOrder(o.order || '')
+
+    return {
+      limit,
+      offset,
+      include,
+      where,
+      order
+    }
+  }
+
+  parseWhere(whereStr) {
+    let query = {}
+    if (!whereStr) {
+      return query
+    }
+    try {
+      query = JSON5.parse(whereStr)
+    } catch(e) {
+      console.log('QUERY PARSE ERROR: ', e)
+    }
+    return query
+  }
+
+  parseOrder(orderStr) {
+    if (!orderStr) {
+      return []
+    }
+    return orderStr.split(',')
+      .map(str => {
+        return str.trim()
+      })
+      .map(prop => {
+        let firstChar = prop[0]
+        if (firstChar === '-') {
+          return [prop.slice(1), 'DESC']
+        } else if (firstChar === '+') {
+          return [prop.slice(1), 'ASC']
+        } else {
+          return prop
+        }
+      })
   }
 
   parseInclude(arr) {
